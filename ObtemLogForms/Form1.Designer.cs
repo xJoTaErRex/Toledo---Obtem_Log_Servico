2725883a183e7c7c58e69f3616b6afe9e4f61e12
JUNIOR ALMEIDA FREITAS <082180035@ftt.cefsa.edu.br>
2022-03-30 09:31:11 -0300

    Adicionar arquivos de projeto.

---
+++ ObtemLogForms/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿
+namespace ObtemLogForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConsultar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNomeMaquina = new System.Windows.Forms.TextBox();
+            this.txtNomeServico = new System.Windows.Forms.TextBox();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.txtCaminhoArquivoLog = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblAvisoRodapé = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnConsultar
+            // 
+            this.btnConsultar.Location = new System.Drawing.Point(282, 161);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(234, 57);
+            this.btnConsultar.TabIndex = 0;
+            this.btnConsultar.Text = "Consultar";
+            this.btnConsultar.UseVisualStyleBackColor = true;
+            this.btnConsultar.Click += new System.EventHandler(this.btnConsultar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(123, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nome da Maquina";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(472, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nome do Serviço";
+            // 
+            // txtNomeMaquina
+            // 
+            this.txtNomeMaquina.Location = new System.Drawing.Point(126, 36);
+            this.txtNomeMaquina.Name = "txtNomeMaquina";
+            this.txtNomeMaquina.Size = new System.Drawing.Size(234, 20);
+            this.txtNomeMaquina.TabIndex = 3;
+            // 
+            // txtNomeServico
+            // 
+            this.txtNomeServico.Location = new System.Drawing.Point(475, 36);
+            this.txtNomeServico.Name = "txtNomeServico";
+            this.txtNomeServico.Size = new System.Drawing.Size(234, 20);
+            this.txtNomeServico.TabIndex = 4;
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(12, 243);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtLog.Size = new System.Drawing.Size(776, 195);
+            this.txtLog.TabIndex = 5;
+            // 
+            // txtCaminhoArquivoLog
+            // 
+            this.txtCaminhoArquivoLog.Location = new System.Drawing.Point(126, 101);
+            this.txtCaminhoArquivoLog.Name = "txtCaminhoArquivoLog";
+            this.txtCaminhoArquivoLog.Size = new System.Drawing.Size(234, 20);
+            this.txtCaminhoArquivoLog.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(123, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Caminho Arquivo";
+            // 
+            // lblAvisoRodapé
+            // 
+            this.lblAvisoRodapé.AutoSize = true;
+            this.lblAvisoRodapé.ForeColor = System.Drawing.Color.Red;
+            this.lblAvisoRodapé.Location = new System.Drawing.Point(13, 445);
+            this.lblAvisoRodapé.Name = "lblAvisoRodapé";
+            this.lblAvisoRodapé.Size = new System.Drawing.Size(0, 13);
+            this.lblAvisoRodapé.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 508);
+            this.Controls.Add(this.lblAvisoRodapé);
+            this.Controls.Add(this.txtCaminhoArquivoLog);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.txtNomeServico);
+            this.Controls.Add(this.txtNomeMaquina);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnConsultar);
+            this.Name = "Form1";
+            this.Text = "Monitor de Serviço";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConsultar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNomeMaquina;
+        private System.Windows.Forms.TextBox txtNomeServico;
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.TextBox txtCaminhoArquivoLog;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblAvisoRodapé;
+    }
+}
+
---
+++ ObtemLogForms/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+
+
+namespace ObtemLogForms
+{
+    public partial class Form1 : Form
+    {
+        /*
+             Programa desenvolvido para extrair informações de uma aplicação do sistema:
+            
+          
+             INFORMAÇÕES do LOG:
+            
+                    "Nome"                                              -> Noma da aplicação
+                    "PID"                                               -> Número de identificação da aplicação 
+                    "Threads"                                           -> Quantidade de Threads
+                    "Handles"                                           -> Quantidade de Handles
+                    "Memória (conjunto de trabalho privado ativo)"      -> Valor de Memória  
+                    "CPU"                                               -> Consumo de CPU em porcentagem
+                    "Tempo de CPU                                       -> Tempo de esxeução da aplicação
+            
+            METODOS:
+                GetEventLogData(DateTime.Today)                         -> Concatenar as informação se há ou não erro(s) na aplicação 
+
+         */
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Método para copiar os logs de ERRO da Aplicação 
+        /// no Event Viewer e inserir concatenado as informações de log 
+        /// do serviço no arquivo txt "dados" e na stirng de retorno.
+        /// </summary>
+        /// <param name="start">
+        /// Retorna uma string
+        /// no Event Viewer rn</param>
+        private static string GetEventLogData(DateTime start, string maquina, string retorno)
+        {
+            var log = new EventLog("Application", maquina);
+            string sourceName = "srvTBROCR";
+            string mensagem;
+
+            log.Source = sourceName;
+            int aux = 0;
+            foreach (EventLogEntry entry in log.Entries)
+            {
+                if ((entry.EntryType == EventLogEntryType.Error) &&
+                    (entry.TimeGenerated >= start) && entry.Source == log.Source)
+                {
+                    aux++;
+                    Console.WriteLine("Error in Event Log:\n" + entry.Message + "\n");
+                }
+
+            }
+            if (aux > 0)
+            {
+                mensagem = $"Há { aux} erros no Event Viewer. Verificar!";
+                File.AppendAllText(@"D:\dados.txt", mensagem.PadLeft(50) + Environment.NewLine);
+                retorno += " " +  mensagem;
+            }
+            else
+            {
+                mensagem = "Não há erros no Event Viewer!!!";
+                File.AppendAllText(@"D:\dados.txt", mensagem.PadLeft(50) + Environment.NewLine);
+                retorno += " " + mensagem;
+            }
+
+            return retorno;
+
+        }
+
+        private void btnConsultar_Click(object sender, EventArgs e)
+        {
+            string servico = "LMPService";
+            string nomeMaquina = "CE230";
+            string caminhoLog = @"D:\dados.txt";
+            string log;
+
+            try
+            {
+
+                if (txtNomeServico.Text != "")
+                {
+                    servico = txtNomeServico.Text;
+                }
+                if (txtNomeMaquina.Text != "")
+                {
+                    nomeMaquina = txtNomeMaquina.Text;
+                }
+                if (txtCaminhoArquivoLog.Text != "")
+                {
+                    caminhoLog = txtCaminhoArquivoLog.Text;
+                }
+
+                txtLog.Text += "Obtendo os Log's de " + servico + "...";
+
+                Process[] processlist = Process.GetProcessesByName(servico);
+
+                foreach (Process theprocess in Process.GetProcessesByName(servico))
+                {
+                    var counter = new PerformanceCounter("Process", "Working Set - Private", servico);
+                    var cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+                    int usage = (int)cpuCounter.NextValue();
+
+                    while (usage == 0 || usage > 100)
+                    {
+                        Thread.Sleep(250);
+                        usage = (int)cpuCounter.NextValue();
+                    }
+
+                    log =   DateTime.Now +
+                            "Nome: ".PadLeft(25) + theprocess.ProcessName.ToString() +
+                            "PID: ".PadLeft(25) + theprocess.Id.ToString() +
+                            "Threads: ".PadLeft(25) + theprocess.Threads.Count +
+                            "Handles: ".PadLeft(25) + theprocess.HandleCount +
+                            "Memória (conjunto de trabalho privado ativo): ".PadLeft(50) + counter.RawValue / 1024 + "K" +
+                            "CPU: ".PadLeft(25) + usage + "%".PadLeft(3) +
+                            "Tempo de CPU: ".PadLeft(25) + theprocess.TotalProcessorTime.TotalSeconds.ToString("N2");
+
+                    //C:\LogAplicacao\dados.txt -- Caminho original 
+                    File.AppendAllText(caminhoLog, log); 
+
+                    txtLog.Text += GetEventLogData(DateTime.Today, nomeMaquina, log);
+                    lblAvisoRodapé.Text = "O arquivo de log dados.txt foi salvo no caminho " + caminhoLog;
+                }
+
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro inesperado, verifique se o serviço esta rodando, erro: " + ex.Message, "Help Caption", MessageBoxButtons.OK,
+                                   MessageBoxIcon.Error);
+            }
+
+        }
+    }
+}
+  
